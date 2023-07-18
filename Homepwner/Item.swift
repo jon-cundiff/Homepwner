e1358424a33233c30ffd3aa825f666731c9eb5d5
jon-cundiff <jon.cundiff@gmail.com>
2023-07-18 07:24:25 -0500

    adds item store

---
+++ Homepwner/Item.swift
@@ -0,0 +1,47 @@
+//
+//  Item.swift
+//  Homepwner
+//
+//  Created by Jon Cundiff on 7/16/23.
+//
+
+import UIKit
+
+class Item: NSObject {
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: Date
+    
+    init(name: String, serialNumber: String?, valueInDollars: Int) {
+        self.name = name
+        self.serialNumber = serialNumber
+        self.valueInDollars = valueInDollars
+        self.dateCreated = Date()
+        
+        super.init()
+    }
+    
+    convenience init(random: Bool = false) {
+        if random {
+            let adjectives = ["Fluffy", "Rusty", "Shiny"]
+            let nouns = ["Bear", "Spork", "Mac"]
+            
+            let randomAdjective = Item.getRandomWordFromList(adjectives)
+            let randomNoun = Item.getRandomWordFromList(nouns)
+            
+            let randomName = "\(randomAdjective) \(randomNoun)"
+            let randomValue = Int(arc4random_uniform(100))
+            let randomSerialNumber = UUID().uuidString.components(separatedBy: "-").first!
+            
+            self.init(name: randomName, serialNumber: randomSerialNumber, valueInDollars: randomValue)
+        } else {
+            self.init(name: "", serialNumber: nil, valueInDollars: 0)
+        }
+    }
+    
+    static func getRandomWordFromList(_ list: [String]) -> String {
+        let idx = arc4random_uniform(UInt32(list.count))
+        return list[Int(idx)]
+    }
+}
--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -8,5 +8,5 @@
 import UIKit
 
 class ItemsViewController: UITableViewController {
-    
+    var itemStore: ItemStore!
 }
